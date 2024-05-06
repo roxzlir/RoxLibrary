eb98a06e3e1f6970edcd173c7fdbb3a501c0f9a3
roxzlir <emil.ch.nordin@gmail.com>
2024-05-06 19:47:46 +0200

    Added DisplayName annotations for Borrow class

--- Models/Borrow.cs
+++ Models/Borrow.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Diagnostics.CodeAnalysis;
 
@@ -13,14 +14,19 @@ namespace RoxLibrary.Models
         [Key]
         public int BorrowId { get; set; }
         [Required]
+        [DisplayName("Checked out Date")]
         public DateTime BorrowDate { get; set;}
         [AllowNull]
+        [DisplayName("Return Date")]
         public DateTime? BorrowReturnDate { get; set; }
+        [DisplayName("Status")]
         public BookStatus BorrowStatus { get; set; }
         [ForeignKey("Customer")]
+        [DisplayName("Customer")]
         public int FkCustomerId { get; set; }
         public Customer? Customer { get; set; }
         [ForeignKey("Book")]
+        [DisplayName("Book")]
         public int FkBookId { get; set; }
         public Book? Book { get; set; }
     }
